7f13579855b546774dec9fd31b676816e7324bbd
Pranit <patilpranit3112@gmail.com>
2024-01-17 21:21:17 +0530

    Notification Alert Completed

--- server.js
+++ server.js
@@ -285,7 +285,7 @@ app.post("/update-profile", verifyJWT, (req, res) => {
     runValidators: true,
   })
     .then(() => {
-      return res.status(200).json({userName});
+      return res.status(200).json({ userName });
     })
     .catch((err) => {
       if (err.code == 11000) {
@@ -864,6 +864,27 @@ app.post("/delete-comment", verifyJWT, (req, res) => {
       return res.status(403).json({ error: "You can not delete the comment" });
     }
   });
+});
+
+app.get("/new-notification", verifyJWT, (req, res) => {
+  const user_id = req.user;
+
+  Notification.exists({
+    notification_for: user_id,
+    seen: false,
+    user: { $ne: user_id },
+  })
+    .then((result) => {
+      if (result) {
+        return res.status(200).json({ new_notification_available: true });
+      } else {
+        return res.status(200).json({ new_notification_available: false });
+      }
+    })
+    .catch((err) => {
+      console.error(`Error in getting notifications ${err.message}`);
+      return res.status(500).json({ err: err.message });
+    });
 });
 
 app.listen(PORT, () => {
