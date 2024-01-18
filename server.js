6415e84fdd568632fc1b4385b99186b0c1c93f32
Pranit <patilpranit3112@gmail.com>
2024-01-18 17:42:46 +0530

    Notification Routes Completed

--- server.js
+++ server.js
@@ -887,6 +887,67 @@ app.get("/new-notification", verifyJWT, (req, res) => {
     });
 });
 
+app.post("/notifications", verifyJWT, (req, res) => {
+  const user_id = req.user;
+
+  let { page, filter, deletedDocCount } = req.body;
+
+  const maxLimit = 10;
+
+  const findQuery = { notification_for: user_id, user: { $ne: user_id } };
+
+  const skipDocs = (page - 1) * maxLimit;
+
+  if (filter != "all") {
+    findQuery.type = filter;
+  }
+
+  if (deletedDocCount) {
+    skipDocs -= deletedDocCount;
+  }
+
+  Notification.find(findQuery)
+    .skip(skipDocs)
+    .limit(maxLimit)
+    .populate("blog", "title blog_id")
+    .populate(
+      "user",
+      "personal_info.fullName personal_info.userName personal_info.profile_img"
+    )
+    .populate("comment", "comment")
+    .populate("replied_on_comment", "comment")
+    .populate("reply", "comment")
+    .sort({ createdAt: -1 })
+    .select("createdAt type seen reply")
+    .then((notifications) => {
+      return res.status(200).json({ notifications });
+    })
+    .catch((err) => {
+      console.error(err.message);
+      return res.status(500).send({ err: err.message });
+    });
+});
+
+app.post("/all-notifications-count", verifyJWT, (req, res) => {
+  const user_id = req.user;
+
+  let { filter } = req.body;
+
+  const findQuery = { notification_for: user_id, user: { $ne: user_id } };
+
+  if (filter != "all") {
+    findQuery.type = filter;
+  }
+
+  Notification.countDocuments(findQuery)
+    .then((count) => {
+      return res.status(200).json({ totalDocs: count });
+    })
+    .catch((err) => {
+      return res.status(500).json({ error: err.message });
+    }); 
+});
+
 app.listen(PORT, () => {
   console.log("Server Running on Port -> " + PORT);
 });
